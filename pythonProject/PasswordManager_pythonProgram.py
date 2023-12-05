f3629963341093456d635ef3bfedecbbff6ffa71
khalif140 <144815806+khalif140@users.noreply.github.com>
2023-12-05 15:50:02 +0000

    Added Anew

---
+++ pythonProject/PasswordManager_pythonProgram.py
@@ -0,0 +1,33 @@
+
+
+
+def Connecter():
+    Email = input(print("Enter email here: "))
+   
+
+    if Email == "Arck@gmail.com":
+        print("Now enter the password")
+        Password = input(print("Enter Password here: "))
+        if Password == "author":
+            print("Welcome")
+    else:
+       print("Not Correct")
+    
+
+# account information from that email and password for how many accounts
+#("Adobe","Retro", "Google", "X")
+    if "welcome":
+        input(print("Do you want to see your accounts are you looking for?:"))
+        if "Adobe":
+            print("Email: Arck@gmail.com")
+            print("Password: Masses")
+        
+        if "Retro":
+            print("Username: MASH01")
+            print("Password: Wellpower")
+
+        if "Google":
+            print("Email: Arck@gmail.com")
+            print("Password: E2007!")
+
+Connecter()
